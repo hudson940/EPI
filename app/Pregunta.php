68a1f09abc1648c5118cbc00bf98e0af9aa457d5
hudson940 <andersonvidal94@gmail.com>
2017-09-03 22:44:41 -0500

    update

--- app/Pregunta.php
+++ app/Pregunta.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Pregunta extends Model
 {
     protected $table='preguntas_generales';
-    protected $fillable=['id','variable','pregunta','option','html','default','class','required','placheholder'];
+    protected $fillable=['id','variable','pregunta','option','html','defecto','class','required','placheholder'];
     public $timestamps= false;
 }
