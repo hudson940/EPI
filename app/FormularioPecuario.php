57fcafd6283cf346e44298d0dd1a7d58f7157e2b
hudson940 <andersonvidal94@gmail.com>
2017-10-08 00:16:09 -0500

    hi

--- app/FormularioPecuario.php
+++ app/FormularioPecuario.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class FormularioPecuario extends Model
 {
-    protected $fillable=['P1_cod_predio', 'P54_otro_pecuario', 'P55_act_pecuaria', 'P56_Area_op', 'P57_machos', 'P57_1_hembras_op', 'P58_final_op', 'P59_duracion_op', ]; 
+    protected $fillable=['P1_cod_pred', 'P54_otro_pecuario', 'P55_act_pecuaria', 'P56_Area_op', 'P57_machos', 'P57_1_hembras_op', 'P58_final_op', 'P59_duracion_op', ]; 
 }
 
